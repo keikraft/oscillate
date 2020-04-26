ce0affb4e3208ed42b0188aad8c881609b93a80b
keira <keira@spotify.com>
2020-04-26 23:28:51 +0200

    canvas aspect ratio

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,11 +1,65 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const CanvasWrapper = styled.canvas`
-  width: 75vw;
-  height: 75vh;
+const CanvasWrapper = styled.div`
+  flex: 1;
+`;
+const StyledCanvas = styled.canvas`
+  width: 1440;
+  height: 900;
 `;
 
 export const Canvas: React.FC = () => {
-  return <CanvasWrapper />;
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
+  const draw = () => {
+    if (canvasRef.current && canvasRef.current.getContext) {
+      var ctx = canvasRef.current.getContext('2d');
+      if (ctx) {
+        ctx.fillStyle = 'orange';
+        // Filled triangle
+        ctx.beginPath();
+        ctx.moveTo(25, 25);
+        ctx.lineTo(105, 25);
+        ctx.lineTo(25, 105);
+        ctx.fill();
+
+        // Stroked triangle
+        ctx.beginPath();
+        ctx.moveTo(125, 125);
+        ctx.lineTo(125, 45);
+        ctx.lineTo(45, 125);
+        ctx.closePath();
+        ctx.stroke();
+      }
+    }
+  };
+
+  const fixCanvasAspectRatio = React.useCallback(() => {
+    if (containerRef.current && canvasRef.current) {
+      const ratio = Math.min(
+        containerRef.current.clientWidth / canvasRef.current.width,
+        containerRef.current.clientHeight / canvasRef.current.height,
+      );
+
+      canvasRef.current.setAttribute('width', `${canvasRef.current.width * ratio}`);
+      canvasRef.current.setAttribute('height', `${canvasRef.current.height * ratio}`);
+      draw();
+    }
+  }, [containerRef, canvasRef]);
+
+  React.useEffect(() => fixCanvasAspectRatio(), [containerRef, canvasRef, fixCanvasAspectRatio]);
+
+  React.useEffect(() => {
+    window.addEventListener('resize', fixCanvasAspectRatio);
+
+    return () => window.removeEventListener('resize', fixCanvasAspectRatio);
+  }, []);
+
+  return (
+    <CanvasWrapper ref={containerRef}>
+      <StyledCanvas ref={canvasRef} />
+    </CanvasWrapper>
+  );
 };
